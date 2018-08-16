e816ba98af76e41b0c172ccf2b40cce05adefa31
Marshall Hampson <mhampson@liveaction.com>
2018-08-16 02:35:26 -0700

    Sending messages to all clients

--- src/main/java/ServerMain.java
+++ src/main/java/ServerMain.java
@@ -1,3 +1,4 @@
+import javax.naming.ldap.SortKey;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
@@ -7,7 +8,11 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Objects;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 /**
  * @author Marshall Hampson
@@ -16,10 +21,13 @@ public class ServerMain {
   private static final int DEFAULT_PORT = 1234;
   
   public static void main (String[] args) {
+    List<Socket> allClientSockets = new CopyOnWriteArrayList<>();
     try (ServerSocket serverSocket = new ServerSocket(DEFAULT_PORT)) {
       while (true) {
         Socket clientSocket = serverSocket.accept();
-        ClientHandlerThread clientHandlerThread = new ClientHandlerThread(clientSocket);
+        allClientSockets.add(clientSocket);
+        ClientHandlerThread clientHandlerThread = 
+          new ClientHandlerThread(clientSocket, allClientSockets);
         clientHandlerThread.start();
       }
     }
@@ -31,10 +39,14 @@ public class ServerMain {
   private static final class ClientHandlerThread extends Thread {
     private static final int CHAR_BUF_SIZE = 256;
     private final Socket clientSocket;
+    private final List<Socket> allSockets;
     
-    private ClientHandlerThread(Socket clientSocket) {
+    private ClientHandlerThread(
+      Socket clientSocket,
+      List<Socket> allSockets) {
       Objects.requireNonNull(clientSocket);
       this.clientSocket = clientSocket;
+      this.allSockets = allSockets;
     }
     
     @Override public void run() {
@@ -46,21 +58,38 @@ public class ServerMain {
           new OutputStreamWriter(clientSocket.getOutputStream(), StandardCharsets.US_ASCII), CHAR_BUF_SIZE)
       ) {
         while (true) {
-          // Handle input
-          if (inputBufferReader.ready()) {
-            String line = inputBufferReader.readLine();
-            if (line != null) {
-              System.out.println(line);
-              if (line.startsWith("BYE")) {
-                break;
+          try {
+            if (inputBufferReader.ready()) {
+              String line = inputBufferReader.readLine();
+              if (line != null) {
+                System.out.println(line);
+                if (line.startsWith("BYE")) {
+                  break;
+                }
+                else if (line.startsWith("SEND")) {
+                  String message = line.substring(4);
+                  for (Socket socket : allSockets) {
+                    try {
+                      socket.getOutputStream().write(message.getBytes(StandardCharsets.US_ASCII));
+                    }
+                    catch (IOException ignored)  {}
+                  }
+                }
               }
             }
+          }
+          catch (IOException e) {
+            e.printStackTrace();
+            break;
           }
         }
         clientSocket.close();
       }
       catch (IOException e) {
-        System.out.println(e.toString());
+        e.printStackTrace();
+      }
+      finally {
+        allSockets.remove(clientSocket);
       }
     }
   }
