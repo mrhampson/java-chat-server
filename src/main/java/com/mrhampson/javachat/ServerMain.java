e604a3dcaf66af5ebd71cb244c6844af50e0957a
Marshall Hampson <mhampson@liveaction.com>
2018-08-16 18:23:31 -0700

    Added basic logging

--- src/main/java/com/mrhampson/javachat/ServerMain.java
+++ src/main/java/com/mrhampson/javachat/ServerMain.java
@@ -1,66 +1,84 @@
+package com.mrhampson.javachat;
+
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Objects;
+import java.util.logging.Logger;
 
 /**
  * @author Marshall Hampson
  */
 public class ServerMain {
   private static final int DEFAULT_PORT = 1234;
+  private static final String LOG_FILE_PATH = "chat-" + 
+    LocalDateTime.now().format(DateTimeFormatter.BASIC_ISO_DATE) + ".log";
   
   public static void main (String[] args) {
     OutboundSocketMessageDispatcher socketMessageDispatcher = new OutboundSocketMessageDispatcher();
-    try (ServerSocket serverSocket = new ServerSocket(DEFAULT_PORT)) {
+    try (
+      ServerSocket serverSocket = new ServerSocket(DEFAULT_PORT);
+      SimpleLogger logger = new SimpleLogger(LOG_FILE_PATH)
+    ) {
       while (true) {
         Socket clientSocket = serverSocket.accept();
         socketMessageDispatcher.registerSocket(clientSocket);
-        ClientHandlerThread clientHandlerThread = new ClientHandlerThread(clientSocket, socketMessageDispatcher);
+        ClientHandlerThread clientHandlerThread = new ClientHandlerThread(clientSocket, socketMessageDispatcher, logger);
         clientHandlerThread.start();
       }
     }
     catch (IOException e) {
-      System.out.println(e.toString());
+      Logger.getGlobal().warning(e.toString());
     }
   }
   
   private static final class ClientHandlerThread extends Thread {
-    private static final int CHAR_BUF_SIZE = 256;
+    private static final int DEFAULT_BUFFER_SIZE = 128;
+    
     private final Socket clientSocket;
     private final OutboundSocketMessageDispatcher socketMessageDispatcher;
+    private final SimpleLogger logger;
     private String username;
     
     private ClientHandlerThread(
       Socket clientSocket,
-      OutboundSocketMessageDispatcher socketMessageDispatcher) {
+      OutboundSocketMessageDispatcher socketMessageDispatcher,
+      SimpleLogger sharedLogger) {
       Objects.requireNonNull(clientSocket);
       Objects.requireNonNull(socketMessageDispatcher);
+      Objects.requireNonNull(sharedLogger);
       this.clientSocket = clientSocket;
       this.socketMessageDispatcher = socketMessageDispatcher;
       this.username = this.clientSocket.getInetAddress().toString();
+      this.logger = sharedLogger;
     }
     
     @Override public void run() {
-      System.out.println("New client thread started: (thread id: " + Thread.currentThread().getId() + ")");
+      logger.log("New client thread started: (thread id: " + Thread.currentThread().getId() + ", client addr: " + 
+        clientSocket.getInetAddress() + ")");
       try (
         BufferedReader inputBufferReader = new BufferedReader(
-          new InputStreamReader(clientSocket.getInputStream(), StandardCharsets.US_ASCII), CHAR_BUF_SIZE)
+          new InputStreamReader(clientSocket.getInputStream(), StandardCharsets.US_ASCII), DEFAULT_BUFFER_SIZE)
       ) {
         while (true) {
           try {
             String line = inputBufferReader.readLine();
             if (line != null) {
-              System.out.println(line);
+              logger.log(line);
               if (line.startsWith("s ") || line.startsWith("SEND ")) {
                 String message = line.substring(line.indexOf(' ') + 1);
                 socketMessageDispatcher.dispatchMessage(username, message);
               }
               else if (line.startsWith("NICK ")) {
                 username = line.substring(line.indexOf(' ') + 1);
+                socketMessageDispatcher.dispatchMessage(username, username + " joined");
               }
               else if (line.startsWith("BYE")) {
                 break;
@@ -79,6 +97,8 @@ public class ServerMain {
       }
       finally {
         socketMessageDispatcher.removeSocket(clientSocket);
+        logger.log("Ended client thread: (thread id: " + Thread.currentThread().getId() + ", client addr: " +
+          clientSocket.getInetAddress() + ")");     
       }
     }
   }
---
+++ src/main/java/com/mrhampson/javachat/SimpleLogger.java
@@ -0,0 +1,57 @@
+/*
+ * SimpleLogger.java
+ * Created on Aug 16, 2018, 6:10 PM
+ *
+ * Copyright 2008-2018 LiveAction, Incorporated. All Rights Reserved.
+ * 3500 W Bayshore Road, Palo Alto, California 94303, U.S.A.
+ *
+ * This software is the confidential and proprietary information
+ * of LiveAction ("Confidential Information").
+ * You shall not disclose such Confidential Information and shall use
+ * it only in accordance with the terms of the license agreement
+ * you entered into with LiveAction.
+ */
+package com.mrhampson.javachat;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+/**
+ * @author Marshall Hampson
+ */
+public class SimpleLogger implements AutoCloseable {
+  private static final int DEFAULT_BUFFER_SIZE = 128;
+  private final BufferedWriter bufferedWriter;
+  private final Executor loggingExecutor = Executors.newSingleThreadExecutor();
+  
+  public SimpleLogger(String logFilePath) throws IOException {
+    File logFile = new File(logFilePath);
+    logFile.createNewFile();
+    bufferedWriter = new BufferedWriter(
+      new OutputStreamWriter(new FileOutputStream(logFile), StandardCharsets.US_ASCII),
+      DEFAULT_BUFFER_SIZE
+    );
+  }
+  
+  public void log(String message) {
+    loggingExecutor.execute(() -> {
+      try {
+        bufferedWriter.write(message);
+        bufferedWriter.write("\n");
+        bufferedWriter.flush();
+      }
+      catch (Exception ignored) {}
+    });
+  }
+  
+  @Override
+  public void close() throws IOException {
+    bufferedWriter.close();
+  }
+}
